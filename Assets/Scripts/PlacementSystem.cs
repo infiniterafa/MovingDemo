31ee376f119f4d151c2c8f15c71d61085b2daf28
infiniterafa <ji.rafa.el05@gmail.com>
2026-02-06 21:23:55 -0600

    V2 inicio
    grid fisica

--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mouseIndicador;
+    private GameObject mouseIndicador, cellIndicator;
 
     [SerializeField]
     private InputManager inputManager;
+    [SerializeField]
+    private Grid grid; 
 
     private void Update()
     {
         //el mouse del juego como un objeto junto con su pos
         Vector3 mouseposition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mouseposition);
         mouseIndicador.transform.position = mouseposition;
+        //convertir posiciones a la grid
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
